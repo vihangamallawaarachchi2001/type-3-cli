0da4d768574a0a0f0c2ab53b6f8d5950ce6dc3f0
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2025-03-16 00:33:16 +0530

    bugfix: version change

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ const banner = figlet.textSync("TYPE-3", {
 
 // Output the banner in cyan color and a description in green.
 console.log(chalk.cyan(banner));
-console.log(chalk.green("Scaffold your backend projects effortlessly! 🚀"));
 
 /**
  * Configure the CLI application using Commander.js.
  * This sets up the CLI's version, description, and commands.
  */
 program
-  .version("1.0.0") // Set the CLI version.
+  .version("0.1.0-beta") // Set the CLI version.
   .description("type-3 CLI - Scaffold your backend projects effortlessly");
 
+  console.log(chalk.green("Scaffold your backend projects effortlessly! 🚀"));
 /**
  * Define the `init` command for the CLI.
  * This command initializes a new backend project by invoking the `initProject` function.
